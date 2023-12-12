11d66f9b781955bd22b74006d9fabc7906a2abf6
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2023-12-12 23:24:06 +0100

    swagger config

--- src/main/java/com/fintech/bannkingapp/BannkingAppApplication.java
+++ src/main/java/com/fintech/bannkingapp/BannkingAppApplication.java
@@ -1,9 +1,34 @@
 package com.fintech.bannkingapp;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Fintech Project",
+				description = "Backend Rest API for a Bank",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Chukwuebuka",
+						email = "cuyanwune@outlook.com",
+						url = "www.github.com/codagott"
+				),
+				license = @License(
+						name = "Coda Gott",
+						url = "www.github.com/codagott"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Java Backend Fintech",
+				url = "www.github.com/codagott"
+		)
+)
 public class BannkingAppApplication {
 
 	public static void main(String[] args) {

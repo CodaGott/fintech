92014bd3ac790454acefca82b4d7f41eae6e2ee6
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2023-12-13 16:04:38 +0100

    transaction entity

---
+++ src/main/java/com/fintech/bannkingapp/entity/Transaction.java
@@ -0,0 +1,24 @@
+package com.fintech.bannkingapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Transaction {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String transactionId;
+    private String transactionType;
+    private BigDecimal transactionAmount;
+    private String accountNumber;
+}
